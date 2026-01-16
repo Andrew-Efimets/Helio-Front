b9a92f6e1440971249f76ec5690298f715634405
Andrew Efimets <mynewspecialpost@gmail.com>
2026-01-16 18:38:04 +0300

    Header build

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AccountView from '@/views/AccountView.vue'
+import ChatsView from '@/views/ChatsView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +10,22 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: AccountView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/chats',
+      name: 'chats',
+      component: ChatsView,
     },
   ],
 })
---
+++ vue/src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input
+      type="tel"
+      placeholder="+7 (900) 000-00-00"
+      pattern="^(\+7|8)[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$"
+      required
+    />
+    <input type="password" name="" id="" placeholder="Пароль" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LoginView',
+})
+</script>
+
+<style scoped></style>
